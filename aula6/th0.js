bb7ce142ea0f81550efd58f98dfc5fad1d930ec6
Thomaz <thomazerick35@gmail.com>
2025-08-21 23:28:57 -0300

    Formatação de numero

--- aula6/th0.js
+++ aula6/th0.js
@@ -1,2 +1,7 @@
-var nome = prompt("Qual é o seu nome?");
-alert(`Olá ${nome}, você está pronto para continuar?`);
+// var nome = prompt("Qual é o seu nome?");
+// alert(`Olá ${nome}, você está pronto para continuar?`);
+
+var n1 = 1500.5
+n1.toFixed(2) //coloca so dois numeros depois do ponto
+n1.toFixed(2).replace('.', ',') // troca "isso" por "aquilo"
+n1.toLocaleString('pt-br', {style: 'currency', /*corrency = dinheiro*/ currency: 'BRL'}) // tranforma em valor real
