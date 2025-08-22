6636d3491251c39148eeb559587d98c7d5dadf0a
Thomaz <thomazerick35@gmail.com>
2025-08-21 21:06:48 -0300

    aula 6

---
+++ aula6/th0.js
@@ -0,0 +1,2 @@
+var nome = prompt("Qual é o seu nome?");
+confirm(`Olá ${nome}, você está pronto para continuar?`);
---
+++ aula6/th1.js
@@ -0,0 +1,8 @@
+var n1 = prompt("digite um número?");
+var n2 = prompt("digite outro número?");
+n1 = parseFloat(n1);
+n2 = parseFloat(n2);
+var n3 = (n1 + n2);
+alert(n3);
+// + para adição 
+// + para concatenação
