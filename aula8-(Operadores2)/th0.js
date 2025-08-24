d4f43de7ecc578aff28ebd7da62cf285106c0302
Thomaz <thomazerick35@gmail.com>
2025-08-23 23:00:24 -0300

    estudo

---
+++ aula8-(Operadores2)/th0.js
@@ -0,0 +1,39 @@
+// Operadores em JavaScript 2
+
+// Relacionais             (Sempre recebe valor Boolean)
+//                        (Pode usar com variavel)
+// 5>4   =  true
+// 7<4   =  false
+// 8>=8  =  true
+// 9<=7  =  false
+// 5==5  =  true
+// 4!=4  =  false
+
+// Indentificadores:
+
+//  5 == 5     true    | 5 != 5     false  |  5 !=  4    true
+//  5 == "5"   true    | 5 != "5"   false  |  5 !=  "4"  true
+//  5 === "5"  false   | 5 !== "5"  true   |  5 !== "4"  true
+//  5 === 5    treu    | 5 !== 5    false  |  5 !== 4    true
+
+// Logicos
+
+//   ! negação       !true  = false
+//                   !false = true
+
+//  && conjunção     true  && true   = true
+//                   true  && false  = false
+//                   false && true   = false
+//                   false && true   = false
+
+//  || disjunção     true  || true   = true
+//                   true  || false  = true
+//                   false || false  = false
+//                   true  || false  = true
+
+// Ternario
+// ? :
+
+// var media = 5.5           media >= 7  ? "aprovado"  :  "reprovado"   =     'reprovado'
+
+// var media1 = 7.5          media1 >= 7 ? "aprovado"  :  "reprovado"   =     'aprovado'
