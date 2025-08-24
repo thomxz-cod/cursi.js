52f549dddbd6609f16028d02369754ca27044acd
Thomaz <thomazerick35@gmail.com>
2025-08-24 14:11:45 -0300

    DOM - Document Object Model

---
+++ moduloC/aula9/th0.js
@@ -0,0 +1,15 @@
+//    DOM
+
+var p1 = document.getElementsByTagName('p')[1]
+var corpo = window.document.body
+
+var d = document.getElementById('clique')
+d.style.color = 'red'
+
+var d1 = document.getElementsByName('msg')[0]
+d1.innerHTML = 'Estou aguardando...'
+d1.style.background = 'green'
+
+var s = document.querySelector('div#select')
+s.style.background = 'red'
+s.style.color = 'black'
