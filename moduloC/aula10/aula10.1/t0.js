4641220ff1476b92451579e09148583f6e8da0da
Thom <thomazerick35@gmail.com>
2025-09-01 15:45:38 -0300

    reorganização

---
+++ moduloC/aula10/aula10.1/t0.js
@@ -0,0 +1,13 @@
+var area = document.querySelector('div#area')
+
+function clicar(){
+    area.style.background = '#aaebf7'
+    area.style.color = 'black'
+}
+function entrar(){
+    area.style.background = 'green'
+}
+function sair(){
+    area.style.background = 'red'
+    area.style.color = 'white'
+}
